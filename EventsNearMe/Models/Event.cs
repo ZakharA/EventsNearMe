735a44e653fab3a774b7afeae8020ca3a02c0562
ZakharA <arsentii.zakharchenko@gmail.com>
2019-09-04 16:39:37 +1000

    added pagination component and event model

---
+++ EventsNearMe/Models/Event.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EventsNearMe.Models
+{
+    public enum EventDate
+    {
+        
+    }
+
+    public enum EventType
+    {
+    }
+
+    public class Event
+    {
+        public string Name { get; set; }
+        public Dictionary<EventDate, DateTime> dates { get; set; }
+        public string Organizer { get; set; }
+        public EventType TypeOfEvent { get; set; }
+        public double price { get; set; }
+        public Location location { get; set; }
+        public string description { get; set; }
+        public List<string> tags { get; set; }
+    }
+}
+
+namespace EventsNearMe
+{
+    public struct Location
+    {
+        string address;
+        string city;
+        int postCode;
+        Dictionary<string, double> coordinates;
+    }
+}
