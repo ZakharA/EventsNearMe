9df2934d11041a73af33437bb05d3ab03ce66c56
ZakharA <arsentii.zakharchenko@gmail.com>
2019-10-08 18:17:52 +1100

    add mapQuest on the create event page

---
+++ EventsNearMe/Scripts/maps.js
@@ -0,0 +1,11 @@
+﻿window.onload = function () {
+    L.mapquest.key = '';
+
+    var map = L.mapquest.map('map', {
+        center: [37.7749, -122.4194],
+        layers: L.mapquest.tileLayer('map'),
+        zoom: 12
+    });
+
+    map.addControl(L.mapquest.control());
+}
