d0fa814959f60067f036586079f1d28fbe1371c6
ZakharA <arsentii.zakharchenko@gmail.com>
2019-10-17 08:05:49 +1100

    add admin and contributor roles

--- EventsNearMe/Startup.cs
+++ EventsNearMe/Startup.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Owin;
+﻿using System;
+using EventsNearMe.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartupAttribute(typeof(EventsNearMe.Startup))]
@@ -9,6 +13,40 @@ namespace EventsNearMe
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            CreateRoles();
+        }
+
+        private void CreateRoles()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+
+            if (!roleManager.RoleExists("Admin"))
+            {
+  
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+                var user = new ApplicationUser { UserName = "admin", Email = "admin@gmail.com" };
+                var result = UserManager.Create(user, "qwerty!12345");
+
+                if (result.Succeeded)
+                {
+                    var result1 = UserManager.AddToRole(user.Id, "Admin");
+                }
+            }
+
+            if (!roleManager.RoleExists("Contributor"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Contributor";
+                roleManager.Create(role);
+
+            }
         }
     }
 }
