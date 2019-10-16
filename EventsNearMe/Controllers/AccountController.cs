0ccf82ba2830288162a0f02ddda556461d32844e
ZakharA <arsentii.zakharchenko@gmail.com>
2019-10-17 08:06:29 +1100

    assign contributor role to new users

--- EventsNearMe/Controllers/AccountController.cs
+++ EventsNearMe/Controllers/AccountController.cs
@@ -155,6 +155,7 @@ namespace EventsNearMe.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
+                    var result1 = await  UserManager.AddToRoleAsync(user.Id, "Contributor");
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
                     
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
